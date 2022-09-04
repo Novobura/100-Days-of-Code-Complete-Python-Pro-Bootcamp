ab58523790a538a50ad8d38c935e0ec6c9e6249c
Novobura <novobura@gmail.com>
2022-09-04 10:32:47 +0900

    Update Day3_treasure_island.py

--- Day3_treasure_island.py
+++ Day3_treasure_island.py
@@ -27,23 +27,23 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-choice1 = input('You walk up to the jungle tree line from the beach, leaving your boat near the surf. The sun beats down from above, a blistering day on the equator. The din of the waves fades as the breath of the jungle, the birds and insects, grows louder. You see a forked path into the foliage. Do you go left or right?').lower()
+choice1 = input('Ye walk up to the jungle tree line from the beach, leaving your boat near the surf. The sun beats down from above, a blistering day on the equator. The din of the waves fades as the breath of the jungle, the birds and insects, grows louder. Ye see a forked path into the foliage. Do ye go left or right?').lower()
 
 
 if choice1 == "left":
-    choice2 = input('You enter the jungle and as you process some yards the sun grows brighter and it becomes easier to see. The sound of animals grows louder and you see a clearing uphead. As you enter the clearing you see fresh water and green grass. As you aporach the water you see the rotting skeletal remains of a long dead pirate. You pick up his cutlass. You progress toward the water. Do you swim or wait?').lower()
+    choice2 = input('Ye enter the jungle and as ye progress some yards the sun grows brighter and it becomes easier to see. The sound of animals grows louder and ye see a clearing uphead. As ye enter the clearing ye see fresh water and green grass. As ye approach the water ye see the rotting skeletal remains of a long dead pirate. Ye pick up his cutlass. Ye progress toward the water. Do you swim or wait?').lower()
     if choice2 == "swim":
-      choice3 = input("You enter the clear fresh water and swim across to the other side, up ahead you see a house with 3 doors. One is red, one is yellow and one blue. Which do you choose?")
+      choice3 = input("Ye enter the clear fresh water and swim across to the other side, up ahead ye see a house with 3 doors. One is red, one is yellow and one blue. Which door do ye choose?")
       if choice3 == "red":
-        print('You open the door and are attacked by a walking rock creature. You swing your cutlass but its too hard, the rock creature bash your head in. Game over.')
+        print("Ye open the door and are attacked by a enmourmous crab creature. Ye swing your cutlass but the creature's carapace is too hard, the crab bashes your head in. Game over.")
       elif choice3 == "yellow":
         print('Ye found the gold debloons. Ye win!')
       elif choice3 == "blue":
-        print('You open the door and are attacked by a walking shark, as it lunges at you, you swing your cutlass and slice off its head. The door slams shut as your attacked and devoured by its young. Game over.')
+        print('Ye open the door and are attacked by a walking shark, as it lunges at you, you swing your cutlass and slice off its head. The door slams shut as your attacked and devoured by its young. Game over.')
       else:
-        print('As you daddle outside, the sun goes down, you hear a moaning and a rotting smell. You are attacked by a hoard of zombies that stumble from the tree line. You swing your cutlas but its too late, the zombies have ye')
+        print('As ye daddle outside, the sun goes down, ye hear a moaning and a rotting smell. Ye are attacked by a hoard of zombies that stumble from the tree line. Ye swing your cutlas and fell the first ghoul but its too late, the zombies have ye. Game over.')
     else:
-      print('You see a shape in the sky like a bird. You see it get bigger and realize its a gargantuan bird. You try to run but it is too late, it swoops down and has ye. Game Over.')
+      print('Ye see a shape in the sky like a bird. Ye see it get bigger and realize its a gargantuan teratorn. You try to run but it is too late, it swoops down and has ye. Game Over.')
   
 else:
-  print('You enter the jungle but as you progress some yards the light of day fades and it becomes difficult to see. The sound of animals grows quiet and you hear a loud snap behind you as if being stalked by a predator. You sprint forward in panic, and you hear a bone shaking roar. Without looking you fall down a cragy precipise into an underground tunnel. The world goes dark. Game over.')
+  print('Ye enter the jungle but as ye progress some yards the light of day fades and it becomes difficult to see. The sound of animals grows quiet and ye hear a loud snap behind ye as if being stalked by a predator. Ye sprint forward in panic, and ye hear a bone shaking roar. Without looking you fall down a cragy precipise into an underground tunnel. The world goes dark. Game over.')
